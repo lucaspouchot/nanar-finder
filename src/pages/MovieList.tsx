48c375f2be1e8ed26f0a217765d6bdddc48b1ed3
lucaspouchot <lucas.pouchot@espridigital.fr>
2023-10-11 22:01:46 +0200

    fix refreshing movies on navigation

--- src/pages/MovieList.tsx
+++ src/pages/MovieList.tsx
@@ -1,6 +1,6 @@
 import { Card } from "../components";
 import { MovieService } from "../services/Movie.service";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Placeholder from "../assets/images/no-image-placeholder-transparent.png";
 import PlaceholderSmall from "../assets/images/no-image-placeholder-transparent-small.png";
 
@@ -12,14 +12,15 @@ export function MovieList({ sort = 'now_playing' }: MovieListProps = {}) {
   const [filter, setFilter] = useState({
     defaultSort: sort
   });
+
+  useEffect(() => {
+    setFilter({ defaultSort: sort });
+  }, [sort]);
+
   const movies = MovieService.useGet(filter);
 
   return (
     <main>
-      <button onClick={() => setFilter({defaultSort: 'now_playing'})}>Recent</button>
-      <button onClick={() => setFilter({defaultSort: 'unpopular'})}>unpopular</button>
-      <button onClick={() => setFilter({defaultSort: 'bottom'})}>Bottom Ratted</button>
-      <button onClick={() => setFilter({defaultSort: 'upcoming'})}>Upcoming</button>
       <section className="flex flex-wrap justify-center gap-4">
         {
           movies.loading && <p>Loading</p>
